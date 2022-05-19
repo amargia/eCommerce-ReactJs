1fd3f510e34ec96ee1c42c68ccf689611a529b25
ecommercemarzuqa <amarzuqa@gmail.com>
2022-05-19 15:48:44 -0400

    Item Collection

--- src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -19,53 +19,11 @@ const ItemDetailContainer = () => {
   } catch (error) {
       console.log(error)
   }
-/*   const myPromise = new Promise((resolve,reject) => {
-    const itemList = [
-      {
-        id:1,
-        categoria: 'mats',
-        producto: 'MAT YOGA 5mm',
-        detalle: 'Mat antideslizante profesional de Yoga. Compuesto de PU premium.',
-        precio: '$40.000',
-        stock: 5,
-        imgUrl: '/imagenes/mat5mm.jpg'
-      },
-      {
-        id:2,
-        categoria: 'mats',
-        producto: 'MAT YOGA 3mm',
-        detalle: 'Mat Yoga antideslizante, fácil de limpiar y liviano. Compuesto de PVC.',
-        precio: '$35.000',
-        stock: 5,
-        imgUrl: '/imagenes/mat3mm.jpg'
-      },
-      {
-        id:3,
-        categoria: 'accesorios',
-        producto: 'LADRILLO ECO',
-        detalle: 'Ladrillo amigable con el medio ambiente de corcho 100%. Provee mayor firmeza y tenacidad que los otros materiales.',
-        precio: '$5.000',
-        stock: 5,
-        imgUrl: '/imagenes/ladrillo.jpg'
-      }
-    ];
-    const item = itemList.find(item => item.id === parseInt(id));
-    setTimeout(() => {
-      resolve(item);
-    });
-  });
-  return myPromise; */
 }
 
   useEffect(() => {
       getItem(id)
     }, [id]);
-
-/*     getItem(id)
-      .then(result => {
-        setItem(result);
-      })
-    }, [id]); */
 
     return (
       <div className='lista'>      
--- src/pages/ItemListContainer/ItemListContainer.jsx
+++ src/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './ItemListContainer.css';
 import { useParams } from 'react-router-dom';
-import { collection, getDocs, getFirestore, query, where, limit, doc } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import db from "../../services/firebase";
 import {ItemList} from '../../components/ItemList/ItemList';
 
@@ -19,14 +19,6 @@ const ItemListContainer = () => {
     } catch (error) {
       console.log(error)
     }
-
-/*   const db = getFirestore();
-  const itemCollection = collection(db, 'items');
-  const q = query(
-    itemCollection
-  );
-
-  return getDocs(q) */
 };
 
 
@@ -34,12 +26,6 @@ const ItemListContainer = () => {
     getProducts(categoriaId)
   }, [categoriaId])
 
-/*   getProducts(categoriaId)
-  .then(snapshot => {
-    setItems(snapshot.docs.map(doc => {
-      return { ...doc.data(), id: doc.id }
-    }));
-  }) */
     return (
       <div>      
         <ItemList itemList={items}/>      
